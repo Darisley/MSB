c7939bb12764edb6d27b7320e1f996bf9e0e275d
Darisley Lopez Martin <darisleyl@gmail.com>
2026-03-03 21:54:06 -0300

    Initial commit: EPI Control System

---
+++ service-worker.js
@@ -0,0 +1,88 @@
+const CACHE_NAME = 'msb-epi-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+  'https://code.jquery.com/jquery-3.6.0.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js'
+];
+
+// Instalar Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache).catch(err => {
+          console.log('Alguns arquivos não puderam ser cacheados:', err);
+          // Continuar mesmo se alguns arquivos falharem
+          return cache.addAll(urlsToCache.filter(url => !url.startsWith('https://cdnjs')));
+        });
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Ativar Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch eventos
+self.addEventListener('fetch', (event) => {
+  // Ignorar requisições que não são GET
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Retornar do cache se disponível
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request)
+          .then((response) => {
+            // Não cachear respostas inválidas
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clonar a resposta
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          })
+          .catch(() => {
+            // Retornar versão cacheada em caso de erro de rede
+            return caches.match(event.request);
+          });
+      })
+  );
+});
+
+// Mensagens
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
